63325394ff9bcb8c2042f7239c48c6d96ec80542
AsadImran <iasad4855@gmail.com>
2026-09-15 07:14:44 +0500

    Fix missing express app declaration in Server.js

--- Server/Server.js
+++ Server/Server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 
+const app = express();
+
 const allowedOrigins = [
     "https://landsidefrontend.netlify.app",
     "http://localhost:5173",
